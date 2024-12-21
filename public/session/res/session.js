2a480f6170c0a3e0b57a1e27e2c3a13c183b0b85
DanielOlivo <vit.masterov@gmail.com>
2024-12-21 19:34:15 +0200

    more bug fixes

--- public/session/res/session.js
+++ public/session/res/session.js
@@ -1,31 +1,163 @@
+const showAnswerPanel = document.querySelector('#show-answer-panel');
+const resultPanel = document.querySelector('#result-panel');
+const [bad, good] = [...resultPanel.querySelectorAll('button')];
+const showAnswerBtn = showAnswerPanel.querySelector('button');
+const exitBtn = document.querySelector('#fin');
+
 const events = {
     stateChange: createEvent(),
-    onShowAnswerCmd: createEvent(),
-    onAnsweringCmd: createEvent(),
+
+    answerSend: createEvent(),
+    reqNext: createEvent(),
+    newItem: createEvent(),
+
+
+
     reqFocus: createEvent(),
+    reqCurrent: createEvent(),
+
+    onAnsweringCmd: createEvent(),
+    onShowAnswerCmd: createEvent(),
+
     finishClick: createEvent(),
-    reqCurrent: createEvent()
 }
 
-setupShowAnswerControl()
-setupAnswerngControl()
-setupFocus()
-setupFinishHandle()
+const socket = getSocket()
 
-setTimeout(setupSocket, 2000);
+// fake
+events.reqNext.addListener('app', () => {
+    // const newCard = {id: 0, front: 'front', back: 'back'};
+    // events.newItem.invoke(newCard);
+
+    socket.emit('next', '', '', async ({card, err}) => {
+        if(err){
+            console.log(err)
+            await wait(5000);
+            window.location.href = 'http://localhost:5000';
+            return
+        }
+        events.newItem.invoke(card);
+    })
+})
+
+
+// panel update
+// inserting new item
+// focusing
+events.newItem.addListener('app', async card => {
+    const {id, front,back} = card
+    insertLowering();
+    insertItem(id, front, back);
+
+    const items = getItems();
+    const last = items[items.length - 1];
+
+    disablePanel(resultPanel);
+    await wait(500);
+    enablePanel(showAnswerPanel);
+    focus(last);
+})
+
+
+events.onShowAnswerCmd.addListener('app', async () => {
+    opening()
+    disablePanel(showAnswerPanel)
+    await wait(500);
+    enablePanel(resultPanel);
+})
+
+// send answer
+// colorize
+const answer = (option) => {
+    console.log('sending to server: ', option)
+    socket.emit('answer', option);
+
+    const item = getLastItem()
+    if(option == 1){
+        item.classList.add('red');
+    }
+    else {
+        item.classList.add('green');
+    }
+
+    events.reqNext.invoke();    
+}
+
+bad.onclick = () => answer(1);
+good.onclick = () => answer(2);
+showAnswerBtn.onclick = () => events.onShowAnswerCmd.invoke();
+fin.onclick = function(e){
+    e.preventDefault()
+    window.location.href = 'http://localhost:5000';
+}
+
+
+events.reqNext.invoke();
+
+
+// setupShowAnswerControl()
+// setupAnswerngControl()
+// setupFocus()
+// setupFinishHandle()
+// setupBackOpeningHandle();
+
+
+
+// // setTimeout(setupSocket, 2000);
 
 // randomGenSetup()
+// test_reqCurrentHandler()
+
+
+// function setupReqNextHandle(){
+    
+// }
+
 // events.stateChange.addListener('debug', console.log);
 
-insertItem(0, 'front', 'back')
-events.stateChange.invoke('q');
+// insertItem(0, 'front', 'back')
+// events.stateChange.invoke('q');
 
+// events.reqCurrent.invoke();
+
+function enablePanel(panel){
+    panel.style.display = 'flex'
+    panel.classList.remove('fade-out');
+    panel.classList.add('fade-in');
+}
+
+async function disablePanel(panel){
+    panel.classList.remove('fade-in');
+    panel.classList.add('fade-out');
+    await wait(300)
+    panel.style.display = 'flex;'
+}
 
 function setupContainer(){
     // const items = [...document.querySelectorAll('.items')];
-
-
 }
+
+function requester(){
+} 
+
+function insertAndFocusAndChangePanel(){
+}
+
+
+function test_reqCurrentHandler(){
+    events.reqCurrent.addListener('debug', () => {
+        insertItem(0, 'front', 'back');
+        const item = document.querySelector('.item');
+        events.reqFocus.invoke(item);
+    })
+}
+
+function getSocket(){
+    const auth = {auth: {token: getToken()}};
+    const socket = io('http://localhost:5000', auth);
+    return socket;
+}
+
 
 async function setupSocket(){
     console.log('socket...');
@@ -78,23 +210,52 @@ function getToken(){
     return document.cookie.match(pattern).groups.token;
 }
 
+function opening(){
+    const label = document.querySelector('.selected .back label');        
+    label.classList.remove('closed')
+    label.classList.add('fade-in');
+}
+
+function setupBackOpeningHandle(){
+    events.onShowAnswerCmd.addListener('opening', opening); 
+}
+
+function focus(item){
+    const scrollable = document.getElementsByClassName('scrollable')[0];
+
+    [...document.querySelectorAll('.selected')].forEach(item => item.classList.remove('selected'));
+    item.classList.add('selected');
+    const position = item.offsetTop - scrollable.offsetTop - scrollable.offsetHeight / 2;
+    scrollable.scrollTo(0, position);
+}
+
 function setupFocus(){
-    const scrollable = document.getElementsByClassName('scrollable')[0]
-
-    function focus(item){
-        const position = item.offsetTop - scrollable.offsetTop - scrollable.offsetHeight / 2;
-        scrollable.scrollTo(0, position);
-    }
-
     events.reqFocus.addListener('scroll', focus);
 }
 
+// function setupSelectionStyleHandle(){
+//     function onFocus(item){
+//         [...document.querySelectorAll('selected')].forEach(it => it.classList.remove('selected'));
+//         item.classList.add('selected');
+//     }
+
+//     events.reqFocus.addListener('selection', onFocus);
+// }
+
 function getItems(){return [...document.querySelectorAll('.item')]}
+
+function getLastItem(){
+    const items = getItems();
+    return items[items.length - 1];
+}
 
 function randomGenSetup(){
     events.onAnsweringCmd.addListener('gen',async (answer) => {
 
         let arr = getItems();
+        if(arr.length == 0){
+            insertItem(0, 'front', 'back');
+        }
         let last = arr[arr.length - 1];
         console.log(last);
         events.reqFocus.invoke(last);
@@ -135,13 +296,22 @@ function insertItem(...args){
 function insertLowering(){
     const container = document.getElementById('container');
     const lastFiller = container.getElementsByClassName('filler')[1];
-    create('div', 'lowering', l => container.insertBefore(l, lastFiller));
+    create('div', '.lowering', l => container.insertBefore(l, lastFiller));
 }
 
 function makeItem(id, front, back){
-    return create('div', 'item', 'id:' + id, item => {
-        create('label', 'side', 'front', 'text:' + front, label => item.appendChild(label));
-        create('label', 'side', 'back', 'text:' + back, label => item.appendChild(label));
+    return create('div', '.item', '#' + id, item => {
+
+        create('div', '.side', '.front', side => {
+            item.appendChild(side);
+            create('label', 'text:' + front, label => side.appendChild(label));
+        })
+
+        create('div', '.side', '.back', side => {
+            item.appendChild(side)
+            create('label', '.closed', 'text:' + back, label => side.appendChild(label));    
+        })
+
     })
 }
 
@@ -168,7 +338,6 @@ function setupAnswerngControl(){
 function setupShowAnswerControl(){
     const panel = document.getElementById('show-answer-panel');
     const btn = document.querySelector('#show-answer-panel button');
-
 
     function run(){
         events.onShowAnswerCmd.invoke();
@@ -207,14 +376,14 @@ function create(tag, ...args){
     for(const arg of args){
 
         if(typeof arg === 'string'){
-            if(arg.startsWith('id:')){
-                el.id = arg.substring(3);
+            if(arg.startsWith('#')){
+                el.id = arg.substring(1);
+            }
+            else if(arg.startsWith('.')){
+                el.classList.add(arg.substring(1));
             }
             else if(arg.startsWith('text:')){
                 el.appendChild(document.createTextNode(arg.substring(5)))
-            }
-            else {
-                el.classList.add(arg);
             }
         }
         else if(typeof arg === 'function'){
--- server.js
+++ server.js
@@ -16,30 +16,49 @@ io.use(middleware);
 io.on('connection', async(socket) => {
     console.log('connect', socket.id);
 
-    socket.on('current', (arg1, arg2, callback) => {
-        const session = sessionController.getBySocketId(socket.id);
-        if(session){
-            callback({card: session.getCurrent()});
-        }
-        else {
-            callback({err: 'not found'})
-        }
-    })
- 
-    socket.on('answer', (answer, arg2, callback) => {
-        console.log('handling answer ', answer);
-        // const session = sessionController.sessions[socket.id];
+    // socket.on('current', (arg1, arg2, callback) => {
+    //     const session = sessionController.getBySocketId(socket.id);
+    //     if(session){
+    //         callback({card: session.getCurrent()});
+    //     }
+    //     else {
+    //         callback({err: 'not found'})
+    //     }
+    // })
+
+    socket.on('next', (a1, a2, callback) => {
         const session = sessionController.getBySocketId(socket.id);
         console.log('session', session)
         if(session){
             const card = session.getNext();
             console.log(card);
-            callback({card: card});
+            callback({card});
         }
         else{
+            console.log('not found')
             callback({err: 'not found'})
         }
-    }) 
+ 
+    })
+
+    socket.on('answer', (answer) => {
+        console.log('user answered: ', answer);
+    })
+ 
+    // socket.on('answer', (answer, arg2, callback) => {
+    //     console.log('handling answer ', answer);
+    //     // const session = sessionController.sessions[socket.id];
+    //     const session = sessionController.getBySocketId(socket.id);
+    //     console.log('session', session)
+    //     if(session){
+    //         const card = session.getNext();
+    //         console.log(card);
+    //         callback({card: card});
+    //     }
+    //     else{
+    //         callback({err: 'not found'})
+    //     }
+    // }) 
 })
  
 module.exports.io = io;
